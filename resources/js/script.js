387500f326d74beddb55952f3537fdc0218efc82
Estelle <estellegregoire0689@gmail.com>
2022-05-04 18:02:35 +0200

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,60 @@
+
+
+/////////////////////////////////////////////////////////////////////
+//Burger menu
+function burgerMenu() {
+  var dropdown = document.getElementById("burgerMenuDropdown");
+  if (dropdown.className === "tabs") {
+    dropdown.className += " showtabs";
+  }
+
+  else {
+    dropdown.className = "tabs";
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////
+//Scroll up arrow, before footer
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+///////////////////SLIDER JS////////////////////////
+document.addEventListener('DOMContentLoaded', function() {
+  var parent = document.querySelector('.splitview'),
+      topPanel = parent.querySelector('.top'),
+      handle = parent.querySelector('.handle'),
+      skewHack = 0,
+      delta = 0;
+
+  // If the parent has .skewed class, set the skewHack var.
+  if (parent.className.indexOf('skewed') != -1) {
+      skewHack = 1000;
+  }
+
+  parent.addEventListener('mousemove', function(event) {
+      // Get the delta between the mouse position and center point.
+      delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+      // Move the handle.
+      handle.style.left = event.clientX + delta + 'px';
+
+      // Adjust the top panel width.
+      topPanel.style.width = event.clientX + skewHack + delta + 'px';
+  });
+});
